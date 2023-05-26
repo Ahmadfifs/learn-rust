d846faa76dc027d797cd3e1a1def2d3aeb283aba
Ahmadfifs <a7-h-m@outlook.com>
2023-05-26 14:39:25 +0300

    create primitve file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,8 @@
+mod primitive;
+mod conditions;
+
 fn main() {
-    // rust primitive types 
-    let number: i32 = 10;
-    let string: &str = "Ahmad";
-    let charachter: char= 'a';
-    let boolean: bool= true;
-    let float: f64 = 0.1;
-    println!("Hello, world! {}", number);
-    println!("Hello, world! {}", string);
-    println!("Hello, world! {}", charachter);
-    println!("Hello, world! {}", boolean);
-    println!("Hello, world! {}", float);
-    println!("Hello, world! {}", add(1, 2));
+    primitive::run();
+    conditions::run();
 }
 
-fn add(n1: i32, n2: i32) -> i32 {
-    return n1 + n2;
-}
